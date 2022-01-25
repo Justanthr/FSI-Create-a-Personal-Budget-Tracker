4a3964c607a90f0afe0bf81d056770b3f67dee06
Jennika Wilson <j.r.a.112100@gmail.com>
2022-01-25 18:58:28 -0500

    added monthly and annually

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ for(let i = 0; i < weeklyExpenseQuestions.length; i++){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
 }
+
+for(let i = 1; i < monthlyExpenseQuestions.length; i++){
+    let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]))
+    monthlyExpenses = monthlyExpenses + answer
+}
